3b2c45082b52cf38f878fc198d40922d089519ec
Vibhor Singh <vibhor.formal@gmail.com>
2020-09-01 00:06:01 +0530

    vending machine

---
+++ qr.js
@@ -0,0 +1,67 @@
+
+const https = require('https');
+var fs=require('fs');
+var content =fs.readFileSync('qr.json','utf-8');
+/*
+* import checksum generation utility
+* You can get this utility from https://developer.paytm.com/docs/checksum/
+*/
+const PaytmChecksum = require('./PaytmChecksum');
+
+var paytmParams = {};
+
+paytmParams.body = {
+    "mid"           : "oqDmCb20264746335612",
+    "orderId"       : "OREDRID98765",
+    "amount"        : "1303.00",
+    "businessType"  : "UPI_QR_CODE",
+    "posId"         : "S12_123"
+};
+
+/*
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), "!9EXJ2MCkf%vWHbO").then(function(checksum){
+
+    paytmParams.head = {
+        "clientId"	: "C11",
+        "version"	: "v1",
+        "signature"	: checksum
+    };
+
+    var post_data = JSON.stringify(paytmParams);
+
+    var options = {
+
+        /* for Staging */
+        hostname: 'securegw-stage.paytm.in',
+
+        /* for Production */
+        // hostname: 'securegw.paytm.in',
+
+        port: 443,
+        path: '/paymentservices/qr/create',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+
+    var response = "";
+    var post_req = https.request(options, function(post_res) {
+        post_res.on('data', function (chunk) {
+            response += chunk;
+        });
+
+        post_res.on('end', function(){
+            //var content =fs.readFileSync(response);
+            //console.log('Response: ', response);
+            console.log(content);
+        });
+    });
+
+    post_req.write(post_data);
+    post_req.end();
+});
---
+++ transactionstatus.js
@@ -0,0 +1,71 @@
+
+const https = require('https');
+/**
+* import checksum generation utility
+* You can get this utility from https://developer.paytm.com/docs/checksum/
+*/
+const PaytmChecksum = require('./PaytmChecksum');
+
+/* initialize an object */
+var paytmParams = {};
+
+/* body parameters */
+paytmParams.body = {
+
+    /* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+    "mid" : "oqDmCb20264746335612",
+
+    /* Enter your order id which needs to be check status for */
+    "orderId" : "111",
+};
+
+/**
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), "YOUR_MERCHANT_KEY").then(function(checksum){
+    /* head parameters */
+    paytmParams.head = {
+
+        /* put generated checksum value here */
+        "signature"	: checksum
+    };
+
+    /* prepare JSON string for request */
+    var post_data = JSON.stringify(paytmParams);
+
+    var options = {
+
+        /* for Staging */
+        hostname: 'securegw-stage.paytm.in',
+
+        /* for Production */
+        // hostname: 'securegw.paytm.in',
+
+        port: 443,
+        path: '/v3/order/status',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+
+    // Set up the request
+    var response = "";
+    var post_req = https.request(options, function(post_res) {
+        post_res.on('data', function (chunk) {
+            response += chunk;
+        });
+
+        post_res.on('end', function(){
+            console.log('Response: ', response);
+        });
+    });
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
+});
+
+        
